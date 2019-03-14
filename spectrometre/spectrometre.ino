623feb331a3b1c18794c0e0f63e66be85f9b7cf5
Thibaud Germain <39822686+Grimnir777@users.noreply.github.com>
2019-03-14 17:04:34 +0100

    Rajout capteur fin de course / init / mesure

--- spectrometre/spectrometre.ino
+++ spectrometre/spectrometre.ino
@@ -4,65 +4,112 @@
 const int stepsPerRevolution = 200; 
 int stepCount = 0;
 
+boolean LancerMesure = LOW;
+int sensorHome ;
+int pinSensorHome;
+
+
+
 String cmd="";
 String dataToSend = "";
-float sensor_val=0.0f;
+float MesurePhotodetecteur=0.0f;
 
+
+int inByte ;
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-SoftwareSerial hc06(7,8);
+
+
+void initialisation (){
+  sensorHome = digitalRead (pinSensorHome);
+  
+  while (sensorHome != LOW) {
+    // Mouvement du moteur
+    Serial.println("clockwise");
+    myStepper.step(10);
+
+    // Qd la commande est fini je remets à 0 les sorties 8,9, 10 et 11
+    sensorHome = digitalRead (pinSensorHome);
+  }
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+}
+
+
+
 
 void setup() {
-  Serial.begin(57600);
-  hc06.begin(9600);
+  //Moteur pas à pas
+  myStepper.setSpeed(60);
+
+  //Moniteur série
+  Serial.begin(9600);
+
+  //Bluetooth
+  Serial1.begin(9600);
+
+  //  Initialisation du capteur photoélectrique
+  pinSensorHome = 28;
+  pinMode(pinSensorHome, INPUT);
+
+  // Initialisation Moteur
+  initialisation(); 
 }
 
 void loop() {
-  while(hc06.available()>0){
-    cmd+=(char)hc06.read();
+
+  while(Serial1.available()>0){
+    cmd+=(char)Serial1.read();
   }
  
  if(cmd!=""){
    Serial.print("Command received : ");
    Serial.println(cmd);
-   
+
    if(cmd=="acq"){
     dataToSend = "";
-    for (int i=0; i <= stepsPerRevolution; i++){
-      if(i%2==0){
-        sensor_val = 0.0f;
-      }
-      else{
-        sensor_val = 5.0f;
-      }
-      dataToSend += String(sensor_val) + ',';
+    initialisation(); 
+    
+    stepCount = 0; 
+    while (stepCount != 80){
+      // Automatic one step at time CW:
+      myStepper.step(1);
+      stepCount++;
+      //Serial.println(stepCount);
       
-     /* myStepper.step(1);
-      stepCount++;*/
-      delay(100);
-   }
-   Serial.println(dataToSend);
-   hc06.print(dataToSend);
-   hc06.print("\n");
-/*
-    //retour à 0 manuel pour l'instant
-    for (int i=0; i <= stepsPerRevolution; i++){
-      myStepper.step(-1);
-      stepCount--;
-      delay(100);
+      dataToSend = String(analogRead (A0)) + '\n';
+      Serial1.print(dataToSend);
+      
+      delay(1000);
+      // Quand la commande est fini je remet à 0 les sorties 8,9, 10 et 11
+      digitalWrite(8, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+
+      // Lancer acquisition
+      
+      Serial.println(MesurePhotodetecteur);
     }
-    digitalWrite(8, LOW); 
-    digitalWrite(9, LOW);  
-    digitalWrite(10, LOW);  
-    digitalWrite(11, LOW); 
-   */ 
+
+    // Remise à 0 du compteur et de la mesure
+    stepCount = 0;
+
+   Serial.println("Sent");
+   Serial1.print("endOfTransmission");
+   
+   initialisation();
+   Serial.println("Reinit OK");
+   
    }
-   else if(cmd=="test"){
-      Serial.println("testOK\n");
-      hc06.print("testOK\n");
+   else if(cmd=="calib"){
+      Serial.println("Calib\n");
+      //TODO
    }
    else{
-       Serial.println("Not a command");
+       Serial.println("Not a command\n");
    }
    cmd="";
  }
